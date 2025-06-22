51887ce596d4bb37de159dfdf120544cdf506c03
NICOGATT <nicogatt1@hotmail.com>
2025-06-22 20:13:28 -0300

    Hice algunos cambios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let botonOscuro = document.getElementById("modoOscuro");
 let isBlack = false;
+let slogan = document.getElementById("slogan");
 
 botonOscuro.addEventListener("click", function() {
     if(isBlack === true) {
@@ -11,12 +12,14 @@ botonOscuro.addEventListener("click", function() {
 
 function toggleLightMode() {
     document.body.style.background = "#e6b9fb"; 
-    document.body.style.color = "black";
+    document.body.style.color = "#564592";
+    slogan.style.borderBottom = "2px dotted #564592";
     isBlack = false;
 };
 
 function toggleDarkMode() {
     document.body.style.background = "#9b59b6";
     document.body.style.color = "#ccc";
+    slogan.style.borderBottom = "2px dotted #ccc"
     isBlack = true;
 }
